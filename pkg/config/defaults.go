1ee9d616af7bc3646ce3beb62c7fe22ab2ed9dcb
budimanjojo <budimanjojo@gmail.com>
2022-06-09 00:06:14 +0700

    feat(config): allow with/without "v" prefix

--- pkg/config/defaults.go
+++ pkg/config/defaults.go
@@ -1,6 +1,10 @@
 package config
 
-import "github.com/talos-systems/talos/pkg/machinery/constants"
+import (
+	"strings"
+
+	"github.com/talos-systems/talos/pkg/machinery/constants"
+)
 
 var (
 	// renovate: depName=siderolabs/talos datasource=github-releases
@@ -11,12 +15,16 @@ func (c TalhelperConfig) k8sVersion() string {
 	if c.KubernetesVersion == "" {
 		return constants.DefaultKubernetesVersion
 	}
-	return c.KubernetesVersion
+	return strings.TrimPrefix(c.KubernetesVersion, "v")
 }
 
 func (c TalhelperConfig) talosVersion() string {
 	if c.TalosVersion == "" {
 		return latestTalosVersion
+	}
+
+	if !strings.HasPrefix(c.TalosVersion, "v") {
+		return "v" + c.TalosVersion
 	}
 	return c.TalosVersion
 }
