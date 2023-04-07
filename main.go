ebe2317466506af3092020a5c3d678d5793832eb
budimanjojo <budimanjojo@gmail.com>
2023-04-07 15:45:01 +0700

    fix: code optimization

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
 	"github.com/budimanjojo/talhelper/cmd"
 )
@@ -10,7 +9,6 @@ import (
 func main() {
 	err := cmd.Execute()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
