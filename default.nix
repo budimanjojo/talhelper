376e9e7620f4e1031c06c1cc9fa33b8c15e0a8ac
budimanjojo <budimanjojo@gmail.com>
2023-05-09 08:37:28 +0700

    feat: update flake

--- default.nix
+++ default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "talhelper";
-  version = "1.7.1";
+  version = "1.7.2";
 
   src = fetchFromGitHub {
     owner = "budimanjojo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-Fy6FDAZurmxSErzMRSkd7RO66e8GIE9b66EBhbCLlbs=";
+    sha256 = "sha256-+MHqexon2be8tRlXtcBdwOHyGetAI8WJDxOniAABEt8=";
   };
 
-  vendorSha256 = "sha256-E6swiu3iwFUseU52BzJHvOWWArHF389wKmlF0uO1I7g=";
+  vendorSha256 = "sha256-EWTGF0K4ecijWo0FDq7zMf7go3AKpH32rhdyuVeaSSM=";
 
   ldflags = [ "-s -w -X github.com/budimanjojo/talhelper/cmd.version=v${version}" ];
 
