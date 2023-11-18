8876ff04430d5a33aa545bc4dfe958d0b19127b2
budimanjojo <budimanjojo@gmail.com>
2023-11-18 15:16:44 +0700

    feat(genurl): generate url with context of `talconfig.yaml`

--- cmd/genurl_installer.go
+++ cmd/genurl_installer.go
@@ -7,14 +7,16 @@ import (
 	"os"
 
 	"github.com/budimanjojo/talhelper/pkg/config"
+	"github.com/budimanjojo/talhelper/pkg/substitute"
 	"github.com/budimanjojo/talhelper/pkg/talos"
+	"github.com/fatih/color"
 	"github.com/siderolabs/image-factory/pkg/schematic"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
 )
 
 var (
 	genurlInstallerCfgFile     string
+	genurlInstallerEnvFile     []string
 	genurlInstallerRegistryURL string
 	genurlInstallerVersion     string
 	genurlInstallerExtensions  []string
@@ -25,45 +27,78 @@ var genurlInstallerCmd = &cobra.Command{
 	Use:   "installer",
 	Short: "Generate URL for Talos installer image",
 	Run: func(cmd *cobra.Command, args []string) {
-		var version string
 		if _, err := os.Stat(genurlInstallerCfgFile); err == nil {
-			var m config.TalhelperConfig
-
-			data, err := os.ReadFile(genurlISOCfgFile)
+			cfgByte, err := os.ReadFile(genurlInstallerCfgFile)
 			if err != nil {
-				log.Fatalf("failed to read Talhelper config file %s, %v", genurlISOCfgFile, err)
+				log.Fatalf("failed to read config file: %s", err)
 			}
 
-			err = yaml.Unmarshal(data, &m)
-			if err != nil {
-				log.Fatalf("failed to unmarshal data: %s", err)
+			if err := substitute.LoadEnvFromFiles(genurlInstallerEnvFile); err != nil {
+				log.Fatalf("failed to load env file: %s", err)
 			}
-			if m.TalosVersion != "" {
-				version = m.TalosVersion
-			} else {
-				version = genurlInstallerVersion
+
+			cfgByte, err = substitute.SubstituteEnvFromByte(cfgByte)
+			if err != nil {
+				log.Fatalf("failed to substitute env: %s", err)
+			}
+
+			cfg, err := config.NewFromByte(cfgByte)
+			if err != nil {
+				log.Fatalf("failed to unmarshal config file: %s", err)
+			}
+
+			errs, warns := cfg.Validate()
+			if len(errs) > 0 || len(warns) > 0 {
+				color.Red("There are issues with your talhelper config file:")
+				grouped := make(map[string][]string)
+				for _, v := range errs {
+					grouped[v.Field] = append(grouped[v.Field], v.Message.Error())
+				}
+				for _, v := range warns {
+					grouped[v.Field] = append(grouped[v.Field], v.Message)
+				}
+				for field, list := range grouped {
+					color.Yellow("field: %q\n", field)
+					for _, l := range list {
+						fmt.Printf(l + "\n")
+					}
+				}
+				if len(errs) > 0 {
+					os.Exit(1)
+				}
+			}
+
+			for _, node := range cfg.Nodes {
+				if node.Schematic != nil {
+					url, err := talos.GetInstallerURL(node.Schematic, genurlInstallerRegistryURL, cfg.GetTalosVersion())
+					if err != nil {
+						log.Fatalf("Failed to generate installer url for %s, %v", node.Hostname, err)
+					}
+					fmt.Printf("%s: %s\n", node.Hostname, url)
+				} else {
+					url, _ := talos.GetInstallerURL(&schematic.Schematic{}, genurlInstallerRegistryURL, cfg.GetTalosVersion())
+					fmt.Printf("%s: %s\n", node.Hostname, url)
+				}
 			}
 		} else if errors.Is(err, os.ErrNotExist) {
-			version = genurlInstallerVersion
+			cfg := &schematic.Schematic{
+				Customization: schematic.Customization{
+					ExtraKernelArgs: genurlInstallerKernelArgs,
+					SystemExtensions: schematic.SystemExtensions{
+						OfficialExtensions: genurlInstallerExtensions,
+					},
+				},
+			}
+
+			url, err := talos.GetInstallerURL(cfg, genurlInstallerRegistryURL, genurlInstallerVersion)
+			if err != nil {
+				log.Fatalf("Failed to generate installer url, %v", err)
+			}
+
+			fmt.Println(url)
 		} else {
 			log.Fatalf("Failed to read Talhelper config file %s, %v", genurlInstallerCfgFile, err)
 		}
-
-		cfg := &schematic.Schematic{
-			Customization: schematic.Customization{
-				ExtraKernelArgs: genurlInstallerKernelArgs,
-				SystemExtensions: schematic.SystemExtensions{
-					OfficialExtensions: genurlInstallerExtensions,
-				},
-			},
-		}
-
-		url, err := talos.GetInstallerURL(cfg, genurlInstallerRegistryURL, version)
-		if err != nil {
-			log.Fatalf("Failed to generate installer url, %v", err)
-		}
-
-		fmt.Println(url)
 	},
 }
 
@@ -71,8 +106,9 @@ func init() {
 	genurlCmd.AddCommand(genurlInstallerCmd)
 
 	genurlInstallerCmd.Flags().StringVarP(&genurlInstallerCfgFile, "config-file", "c", "talconfig.yaml", "File containing configurations for talhelper")
+	genurlInstallerCmd.Flags().StringSliceVar(&genurlInstallerEnvFile, "env-file", []string{"talenv.yaml", "talenv.sops.yaml", "talenv.yml", "talenv.sops.yml"}, "List of files containing env variables for config file")
 	genurlInstallerCmd.Flags().StringVarP(&genurlInstallerRegistryURL, "registry-url", "r", "factory.talos.dev/installer", "Registry url of the image")
 	genurlInstallerCmd.Flags().StringVarP(&genurlInstallerVersion, "version", "v", config.LatestTalosVersion, "Talos version to generate (defaults to latest Talos version)")
-	genurlInstallerCmd.Flags().StringSliceVarP(&genurlInstallerExtensions, "extension", "e", []string{}, "Official extension image to be included in the image")
-	genurlInstallerCmd.Flags().StringSliceVarP(&genurlInstallerKernelArgs, "kernel-arg", "k", []string{}, "Kernel arguments to be passed to the image kernel")
+	genurlInstallerCmd.Flags().StringSliceVarP(&genurlInstallerExtensions, "extension", "e", []string{}, "Official extension image to be included in the image (ignored when talconfig.yaml is found)")
+	genurlInstallerCmd.Flags().StringSliceVarP(&genurlInstallerKernelArgs, "kernel-arg", "k", []string{}, "Kernel arguments to be passed to the image kernel (ignored when talconfig.yaml is found)")
 }
--- cmd/genurl_iso.go
+++ cmd/genurl_iso.go
@@ -7,14 +7,16 @@ import (
 	"os"
 
 	"github.com/budimanjojo/talhelper/pkg/config"
+	"github.com/budimanjojo/talhelper/pkg/substitute"
 	"github.com/budimanjojo/talhelper/pkg/talos"
+	"github.com/fatih/color"
 	"github.com/siderolabs/image-factory/pkg/schematic"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
 )
 
 var (
 	genurlISOCfgFile     string
+	genurlISOEnvFile     []string
 	genurlISORegistryURL string
 	genurlISOVersion     string
 	genurlISOTalosMode   string
@@ -27,44 +29,77 @@ var genurlISOCmd = &cobra.Command{
 	Use:   "iso",
 	Short: "Generate URL for Talos ISO image",
 	Run: func(cmd *cobra.Command, args []string) {
-		var version string
 		if _, err := os.Stat(genurlISOCfgFile); err == nil {
-			var m config.TalhelperConfig
+			cfgByte, err := os.ReadFile(genurlISOCfgFile)
+			if err != nil {
+				log.Fatalf("failed to read config file: %s", err)
+			}
 
-			data, err := os.ReadFile(genurlISOCfgFile)
-			if err != nil {
-				log.Fatalf("failed to read Talhelper config file %s, %v", genurlISOCfgFile, err)
+			if err := substitute.LoadEnvFromFiles(genurlISOEnvFile); err != nil {
+				log.Fatalf("failed to load env file: %s", err)
 			}
-			err = yaml.Unmarshal(data, &m)
+
+			cfgByte, err = substitute.SubstituteEnvFromByte(cfgByte)
 			if err != nil {
-				log.Fatalf("failed to unmarshal data: %s", err)
+				log.Fatalf("failed to substitute env: %s", err)
 			}
-			if m.TalosVersion != "" {
-				version = m.TalosVersion
-			} else {
-				version = genurlISOVersion
+
+			cfg, err := config.NewFromByte(cfgByte)
+			if err != nil {
+				log.Fatalf("failed to unmarshal config file: %s", err)
+			}
+
+			errs, warns := cfg.Validate()
+			if len(errs) > 0 || len(warns) > 0 {
+				color.Red("There are issues with your talhelper config file:")
+				grouped := make(map[string][]string)
+				for _, v := range errs {
+					grouped[v.Field] = append(grouped[v.Field], v.Message.Error())
+				}
+				for _, v := range warns {
+					grouped[v.Field] = append(grouped[v.Field], v.Message)
+				}
+				for field, list := range grouped {
+					color.Yellow("field: %q\n", field)
+					for _, l := range list {
+						fmt.Printf(l + "\n")
+					}
+				}
+				if len(errs) > 0 {
+					os.Exit(1)
+				}
+			}
+
+			for _, node := range cfg.Nodes {
+				if node.Schematic != nil {
+					url, err := talos.GetISOURL(node.Schematic, genurlISORegistryURL, cfg.GetTalosVersion(), genurlISOTalosMode, genurlISOArch)
+					if err != nil {
+						log.Fatalf("Failed to generate installer url for %s, %v", node.Hostname, err)
+					}
+					fmt.Printf("%s: %s\n", node.Hostname, url)
+				} else {
+					url, _ := talos.GetISOURL(&schematic.Schematic{}, genurlISORegistryURL, cfg.GetTalosVersion(), genurlISOTalosMode, genurlISOArch)
+					fmt.Printf("%s: %s\n", node.Hostname, url)
+				}
 			}
 		} else if errors.Is(err, os.ErrNotExist) {
-			version = genurlISOVersion
+			cfg := &schematic.Schematic{
+				Customization: schematic.Customization{
+					ExtraKernelArgs: genurlISOKernelArgs,
+					SystemExtensions: schematic.SystemExtensions{
+						OfficialExtensions: genurlISOExtensions,
+					},
+				},
+			}
+			url, err := talos.GetISOURL(cfg, genurlISORegistryURL, genurlISOVersion, genurlISOTalosMode, genurlISOArch)
+			if err != nil {
+				log.Fatalf("Failed to generate installer url, %v", err)
+			}
+
+			fmt.Println(url)
 		} else {
 			log.Fatalf("Failed to read Talhelper config file %s, %v", genurlISOCfgFile, err)
 		}
-
-		cfg := &schematic.Schematic{
-			Customization: schematic.Customization{
-				ExtraKernelArgs: genurlISOKernelArgs,
-				SystemExtensions: schematic.SystemExtensions{
-					OfficialExtensions: genurlISOExtensions,
-				},
-			},
-		}
-
-		url, err := talos.GetISOURL(cfg, genurlISORegistryURL, version, genurlISOTalosMode, genurlISOArch)
-		if err != nil {
-			log.Fatalf("Failed to generate installer url, %v", err)
-		}
-
-		fmt.Println(url)
 	},
 }
 
@@ -72,10 +107,11 @@ func init() {
 	genurlCmd.AddCommand(genurlISOCmd)
 
 	genurlISOCmd.Flags().StringVarP(&genurlISOCfgFile, "config-file", "c", "talconfig.yaml", "File containing configurations for talhelper")
+	genurlISOCmd.Flags().StringSliceVar(&genurlISOEnvFile, "env-file", []string{"talenv.yaml", "talenv.sops.yaml", "talenv.yml", "talenv.sops.yml"}, "List of files containing env variables for config file")
 	genurlISOCmd.Flags().StringVarP(&genurlISORegistryURL, "registry-url", "r", "https://factory.talos.dev/image", "Registry url of the image")
 	genurlISOCmd.Flags().StringVarP(&genurlISOVersion, "version", "v", config.LatestTalosVersion, "Talos version to generate (defaults to latest Talos version)")
 	genurlISOCmd.Flags().StringVarP(&genurlISOTalosMode, "talos-mode", "m", "metal", "Talos runtime mode to generate URL")
 	genurlISOCmd.Flags().StringVarP(&genurlISOArch, "arch", "a", "amd64", "CPU architecture support of the image")
-	genurlISOCmd.Flags().StringSliceVarP(&genurlISOExtensions, "extension", "e", []string{}, "Official extension image to be included in the image")
-	genurlISOCmd.Flags().StringSliceVarP(&genurlISOKernelArgs, "kernel-arg", "k", []string{}, "Kernel arguments to be passed to the image kernel")
+	genurlISOCmd.Flags().StringSliceVarP(&genurlISOExtensions, "extension", "e", []string{}, "Official extension image to be included in the image (ignored when talconfig.yaml is found)")
+	genurlISOCmd.Flags().StringSliceVarP(&genurlISOKernelArgs, "kernel-arg", "k", []string{}, "Kernel arguments to be passed to the image kernel (ignored when talconfig.yaml is found)")
 }
