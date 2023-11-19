cbcd1f9096a4657631903a14f5b7e7aa06d18531
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-11-19 12:28:32 +0000

    Cleanup code

--- pkg/generate/command.go
+++ pkg/generate/command.go
@@ -7,26 +7,31 @@ import (
 	"github.com/budimanjojo/talhelper/pkg/talos"
 )
 
-func GenerateCommand(c *config.TalhelperConfig, outDir string, generateForNode string, generateApply bool, generateUpgrade bool) error {
-	if !generateApply && !generateUpgrade {
-		fmt.Printf("Must select one of `--apply` of `--upgrade`\n")
-		return nil
+func GenerateCommand(cfg *config.TalhelperConfig, gencommandFlagNode string, gencommandFlagApply bool, gencommandFlagUpgrade bool) error {
+	if !gencommandFlagApply && !gencommandFlagUpgrade {
+		return fmt.Errorf("Must select one of `--apply` of `--upgrade`\n")
 	}
 
-	for _, node := range c.Nodes {
-		var isSelectedNode = generateForNode != "" && generateForNode == node.IPAddress
-		if generateForNode == "" || isSelectedNode {
-			if generateApply {
-				fileName := outDir + "/" + c.ClusterName + "-" + node.Hostname + ".yaml"
-				fmt.Printf("talosctl apply-config --talosconfig %s/talosconfig --nodes %s --file %s --insecure;\n", outDir, node.IPAddress, fileName)
+	for _, node := range cfg.Nodes {
+		isSelectedNode := ( (gencommandFlagNode != "") && (gencommandFlagNode == node.IPAddress) )
+		allNodesSelected := (gencommandFlagNode == "")
+
+		if allNodesSelected || isSelectedNode {
+			if gencommandFlagApply {
+				fileName := outDir + "/" + cfg.ClusterName + "-" + node.Hostname + ".yaml"
+				fmt.Printf("talosctl apply-config --talosconfig %s/talosconfig --nodes %s --file %s;\n", outDir, node.IPAddress, fileName)
 			}
 			
-			if generateUpgrade {
-				var image, err = talos.GetInstallerURL(node.Schematic, "factory.talos.dev", c.TalosVersion)
-				if err != nil {
-					log.Fatalf("Failed to generate installer url, %v", err)
+			if gencommandFlagUpgrade {
+				if node.Schematic != nil {
+					url, err := talos.GetInstallerURL(node.Schematic, gencommandInstallerRegistryURL, cfg.GetTalosVersion())
+					if err != nil {
+						return fmt.Errorf("Failed to generate installer url for %s, %v", node.Hostname, err)
+					}
+				} else {
+					url, _ := talos.GetInstallerURL(&schematicfg.Schematic{}, gencommandInstallerRegistryURL, cfg.GetTalosVersion())
 				}
-				fmt.Printf("talosctl upgrade --talosconfig %s/talosconfig --nodes %s --image %s;\n", outDir, node.IPAddress, image)
+				fmt.Printf("talosctl upgrade --talosconfig %s/talosconfig --nodes %s --image %s;\n", outDir, node.IPAddress, url)
 			}
 		}
 	}
