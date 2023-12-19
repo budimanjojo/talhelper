7eb08a910b07fd9e577de3d4a3c94878d34bd1e4
GoProSlowYo <68455785+goproslowyo@users.noreply.github.com>
2023-12-18 21:57:00 -0800

    Add new output formats to flags.go

--- hack/version-schema-gen/flags.go
+++ hack/version-schema-gen/flags.go
@@ -6,14 +6,20 @@ var minimal bool
 
 var purge bool
 
+var onlyVersions bool
+var version string
+
 var trimRegistry bool
 var trimSha256 bool
 var trimTag bool
 
 func init() {
-	flag.BoolVar(&minimal, "minimal", false, "output minimal json consisting of only the org/repo, e.g. 'siderolabs/amd-ucode' -- flag is mutually exclusive from '-trimRegistry' and '-trimSha256'.")
+	flag.BoolVar(&minimal, "minimal", false, "output minimal json consisting of only the org/repo, e.g. 'siderolabs/amd-ucode' -- flag is mutually exclusive to '-trimRegistry', '-trimSha256', and '-trimTag'.")
 
 	flag.BoolVar(&purge, "purgecache", false, "purge the cache file")
+
+	flag.BoolVar(&onlyVersions, "onlyVersions", false, "only output the versions, e.g. 'v1.5.5' -- flag is mutually exclusive to '-trimRegistry', '-trimSha256', and '-trimTag'.")
+	flag.StringVar(&version, "version", "", "only output the version specified, e.g. 'v1.5.5' -- flag is mutually exclusive to '-trimRegistry', '-trimSha256', and '-trimTag'.")
 
 	flag.BoolVar(&trimRegistry, "trimRegistry", false, "trim the sha256 suffix e.g. 'ghcr.io/siderolabs/amd-ucode:v1.2.0@sha256:1234567890abcdef' -> 'ghcr.io/siderolabs/amd-ucode:v1.2.0'")
 	flag.BoolVar(&trimSha256, "trimSha256", false, "trim the registry prefix, e.g. 'ghcr.io/siderolabs/extensions/siderolabs/amd-ucode:v1.2.0@sha256:1234567890abcdef' -> 'siderolabs/amd-ucode:v1.2.0@sha256:1234567890abcdef'")
