3c5e7592eaba124b8ebcbc659145f831e82e76bf
budimanjojo <budimanjojo@gmail.com>
2024-05-18 11:56:12 +0700

    chore: bump go version in flake
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- flake.nix
+++ flake.nix
@@ -18,16 +18,16 @@
         {
           _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
-            # overlays = [
-            #   (final: prev: {
-            #     go_1_22 = prev.go_1_22.overrideAttrs (old: {
-            #       src = prev.fetchurl {
-            #         url = "https://go.dev/dl/go1.22.2.src.tar.gz";
-            #         hash = "sha256-N06oKyiexzjpaCZ8rFnH1f8YD5SSJQJUeEsgROkN9ak=";
-            #       };
-            #     });
-            #   })
-            # ];
+            overlays = [
+              (final: prev: {
+                go_1_22 = prev.go_1_22.overrideAttrs (old: {
+                  src = prev.fetchurl {
+                    url = "https://go.dev/dl/go1.22.3.src.tar.gz";
+                    hash = "sha256-gGSO80+QMZPXKlnA3/AZ9fmK4MmqE63gsOy/+ZGnb2g=";
+                  };
+                });
+              })
+            ];
           };
           packages.default = pkgs.callPackage ./default.nix { };
           devShells.default =
