2797a3804b5f41d296b309b81ad2e17ce4fd1314
Elliot Roberts <elliot0000101@gmail.com>
2024-09-08 07:54:13 -0700

    add test for some comment stripping corner cases

--- pkg/substitute/envsubst_test.go
+++ pkg/substitute/envsubst_test.go
@@ -83,3 +83,26 @@ default: default value
 		}
 	}
 }
+
+func TestComplexCommentStrip(t *testing.T) {
+	file := `a1: '123!@# not a comment'
+a2: |
+    # not a comment
+b1: '"' # comment
+b2: "'" # comment
+b3: "\"" # comment
+`
+
+	expected := `a1: '123!@# not a comment'
+a2: |
+    # not a comment
+b1: '"'
+b2: "'"
+b3: "\""
+`
+
+	result, _ := SubstituteEnvFromByte([]byte(file))
+	if expected != string(result) {
+		t.Errorf("got %s, want %s", string(result), expected)
+	}
+}
