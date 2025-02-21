ec47c88b385a9e9e5bd12f748f0091c3cbf68a2b
budimanjojo <budimanjojo@gmail.com>
2025-02-21 13:24:53 +0700

    feat(gencommand): add shorthand for `--env-file` flag to match genconfig
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- cmd/gencommand.go
+++ cmd/gencommand.go
@@ -22,7 +22,7 @@ func init() {
 	rootCmd.AddCommand(gencommandCmd)
 	gencommandCmd.PersistentFlags().StringVarP(&gencommandCfgFile, "config-file", "c", "talconfig.yaml", "File containing configurations for talhelper")
 	gencommandCmd.PersistentFlags().StringVarP(&gencommandOutDir, "out-dir", "o", "./clusterconfig", "Directory that contains the generated config files to apply.")
-	gencommandCmd.PersistentFlags().StringSliceVar(&gencommandEnvFile, "env-file", []string{"talenv.yaml", "talenv.sops.yaml", "talenv.yml", "talenv.sops.yml"}, "List of files containing env variables for config file")
+	gencommandCmd.PersistentFlags().StringSliceVarP(&gencommandEnvFile, "env-file", "e", []string{"talenv.yaml", "talenv.sops.yaml", "talenv.yml", "talenv.sops.yml"}, "List of files containing env variables for config file")
 	gencommandCmd.PersistentFlags().StringSliceVar(&gencommandExtraFlags, "extra-flags", []string{}, "List of additional flags that will be injected into the generated commands.")
 	gencommandCmd.PersistentFlags().StringVarP(&gencommandNode, "node", "n", "", "A specific node to generate the command for. If not specified, will generate for all nodes.")
 	_ = helpers.MakeNodeCompletion(gencommandCmd)
