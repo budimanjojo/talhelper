c37699bd869ddac75c74e29d9f35bc59449e5635
budimanjojo <budimanjojo@gmail.com>
2025-05-03 12:09:49 +0700

    fix(schema): fix json schema for `volumes` and `userVolumes`
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- pkg/config/schema.go
+++ pkg/config/schema.go
@@ -33,9 +33,41 @@ type IngressConfigWrapper struct {
 	Except string `yaml:"except" jsonschema:"description=Source subnet to exclude from the subnet"`
 }
 
+type DiskSelectorWrapper struct {
+	Match string `yaml:"match" jsonschema:"description=The Common Expression Language (CEL) expression to match the disk"`
+}
+
+type ProvisioningSpecWrapper struct {
+	DiskSelectorSpec    DiskSelectorWrapper `yaml:"diskSelector" jsonschema:"description=The disk selector expression"`
+	ProvisioningGrow    bool                `yaml:"grow" jsonschema:"description=Should the volume grow to the size of the disk (if possible)"`
+	ProvisioningMinSize string              `yaml:"minSize" jsonschema:"description=The minimum size of the volume,example=2.5GiB"`
+	ProvisioningMaxSize string              `yaml:"maxSize" jsonschema:"description=The maximum size of the volume, if not specified the volume can grow to the size of the disk,example=50GiB"`
+}
+
+type FilesystemSpecWrapper struct {
+	FilesystemType string `yaml:"type" jsonschema:"default=xfs,description=Filesystem type,enum=ext4,enum=xfs"`
+}
+
 func (Node) JSONSchemaProperty(prop string) any {
 	if prop == "installDiskSelector" {
 		return &InstallDiskSelectorWrapper{}
+	}
+	return nil
+}
+
+func (UserVolume) JSONSchemaProperty(prop string) any {
+	if prop == "provisioning" {
+		return &ProvisioningSpecWrapper{}
+	}
+	if prop == "filesystem" {
+		return &FilesystemSpecWrapper{}
+	}
+	return nil
+}
+
+func (Volume) JSONSchemaProperty(prop string) any {
+	if prop == "provisioning" {
+		return &ProvisioningSpecWrapper{}
 	}
 	return nil
 }
